d7f447c0f0deb06a6cd15a6cf90d58aca2e40c17
Poisk <vpisotski@gmail.com>
2018-07-01 18:36:53 -0700

    got rid of tempstorage

--- server/previews.js
+++ server/previews.js
@@ -1,54 +1,47 @@
 const express = require('express');
 const clientMongo = require('../database/mindex.js');
 const clientRedis = require('../database/rindex.js');
-const help = require('../helpers/serverHelpers.js');
-let ts = require('./tempStorage.js');
+const create = require('../helpers/serverHelpers.js');
 
 const router = express.Router();
 
-const roomIdAdjustment = 0;
-
-const getQueryParams = ({ pageonly, start, limit }) => {
-  const result = {};
-  result.totalNumberResults = ts.totalNumberResults;
-  console.log(ts.allQueryReviews.length);
-  if (!parseInt(pageonly, 10)) {
-    result.roomInfo = ts.roomInfo[0];
-  }
+const reviewsForCurrPage = ({ pageonly, start, limit }, roomBundle) => {
   const index = parseInt(start, 10);
   const end = parseInt(limit, 10) + index;
-  result.reviews = ts.allQueryReviews.slice(index, end);
-  return result;
+  return {
+    totalNumberResults: roomBundle.totalNumberResults,
+    roomInfo: roomBundle.roomInfo[0],
+    reviews: roomBundle.reviews.slice(index, end),
+  };
 };
 
 const cache = async (req, res, next) => {
   try {
     const { roomId } = req.params;
-    let data = await clientRedis.get(roomId);
-    if (data != null) {
-      data = JSON.parse(data);
-      ts = data;
-      res.status(200).json(getQueryParams(req.query));
+    let roomBundle = await clientRedis.get(roomId);
+    if (roomBundle != null) {
+      roomBundle = JSON.parse(roomBundle);
+      res.status(200).json(reviewsForCurrPage(req.query, roomBundle));
     } else {
       next();
     }
   } catch (err) {
-    next();
+    next(err);
   }
 };
 
 router.get('/:roomId', cache, async (req, res, next) => {
   try {
     let { roomId } = req.params;
-    roomId = parseInt(roomId, 10) + roomIdAdjustment;
+    roomId = parseInt(roomId, 10);
     const roomInfo = clientMongo.getRoomInfo(roomId);
     const reviews = clientMongo.getReviews(roomId);
-    [ts.roomInfo, ts.allQueryReviews] = [await roomInfo, await reviews];
-    ts.totalNumberResults = ts.allQueryReviews.length;
-    ts.roomInfo[0].totalNumberReviews = ts.allQueryReviews.length;
-    const roomReviews = JSON.stringify(getQueryParams(req.query));
-    clientRedis.setex(roomId, 100, roomReviews);
-    res.status(200).json(getQueryParams(req.query));
+    const roomBundle = {};
+    [roomBundle.roomInfo, roomBundle.reviews] = [await roomInfo, await reviews];
+    roomBundle.totalNumberResults = roomBundle.reviews.length;
+    roomBundle.roomInfo[0].totalNumberReviews = roomBundle.reviews.length;
+    clientRedis.setex(roomId, 100, JSON.stringify(roomBundle));
+    res.status(200).json(reviewsForCurrPage(req.query, roomBundle));
   } catch (err) {
     next(err);
   }
@@ -57,7 +50,7 @@ router.get('/:roomId', cache, async (req, res, next) => {
 // router.post('/:roomId', async (req, res, next) => {
 //   try {
 //     const { roomId } = req.params;
-//     const review = help.newReview(roomId);
+//     const review = create.newReview(roomId);
 //     roomId = parseInt(roomId, 10) + roomIdAdjustment;
 //     const room = db.getRoomById(roomId);
 //     const reviews = db.getReviewsByRoomId({ roomId });
@@ -77,7 +70,7 @@ router.get('/:roomId', cache, async (req, res, next) => {
 router.post('/post/:roomId', async (req, res, next) => {
   try {
     const { roomId } = req.params;
-    const review = help.newReview(roomId);
+    const review = create.newReview(roomId);
     await clientMongo.postReviews(review);
     const roomReviews = await clientMongo.getReviews(roomId);
     res.status(200).json(roomReviews);
